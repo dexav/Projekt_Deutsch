105fa66b8f95dd7d223f7c70127b711fce2914d4
dexav <110892637+dexav@users.noreply.github.com>
2022-10-27 09:48:54 +0200

    Update Code.cs

--- Code.cs
+++ Code.cs
@@ -5,17 +5,15 @@ namespace Projekt_Deutsch
 {
     internal class Program
    
-     {
-
-            var item = new List<string>();
+    {   var item = new List<string>();
             var data = new List<List<string>>();
             var file = @"C:\Users\xnurs\Desktop\Verben.txt";
 
             data = ReadFromFile(file);
             
-            string[][] dataArray = data.Select(a => a.ToArray()).ToArray(); 
+            string[][] dataArray = data.Select(a => a.ToArray()).ToArray(); // Konvertierung in ein Array
 
-         
+            //Ausgabe des Arrays (als Alternative zur List)
             for (int i = 0; i < dataArray.Length; i++)
             {
                 for (int j = 0; j < dataArray[i].Length; j++)
@@ -25,6 +23,22 @@ namespace Projekt_Deutsch
             }
 
 
+
+        }
+        static List<List<string>> ReadFromFile(string filename)
+        {
+            var fileItem = new List<string>();
+            var fileData = new List<List<string>>();
+            using (var sr= new StreamReader(filename))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    fileItem = line.Split('\t').ToList();
+                    fileData.Add(fileItem);
+                }
+                return fileData;
+            }
 
         }
         static List<List<string>> ReadFromFile(string filename)
